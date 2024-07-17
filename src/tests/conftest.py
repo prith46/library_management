ef8a3eb23428947ab8716e903a8caeccdac846fa
prith46 <prithiviraj17a@gmail.com>
2024-07-17 21:55:27 +0530

    updating book test

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,7 +4,7 @@ from src.tests.database import database_function as db
 
 
 @pytest.fixture(scope="module")
-def load_test_data():
+def load_book_test_data():
     bk = Book()
     db.execute_test_sql('E:/Projects/library_management/src/tests/database/book_database.sql')
     yield bk
--- src/tests/test_files/test_book.py
+++ src/tests/test_files/test_book.py
@@ -1,21 +1,21 @@
 from src.tests.database import database_function as db
-from src.tests.conftest import load_test_data
+from src.tests.conftest import load_book_test_data
 
 
-def test_add_book(load_test_data):
-    load_test_data.add_book(101, 'The Mindset', 'Thomas', 2012, 7728545662, 12)
+def test_add_book(load_book_test_data):
+    load_book_test_data.add_book(101, 'The Mindset', 'Thomas', 2012, 7728545662, 12)
     data = db.get_one_data(101)
     assert data[0] == 101
     assert data[1] == 'The Mindset'
 
 
-def test_get_all_books(load_test_data):
-    data = load_test_data.get_all_books()
+def test_get_all_books(load_book_test_data):
+    data = load_book_test_data.get_all_books()
     print(data)
 
 
-def test_delete_book(load_test_data):
-    load_test_data.delete_book('The Mindset')
+def test_delete_book(load_book_test_data):
+    load_book_test_data.delete_book('The Mindset')
     data = db.get_one_data(101)
     assert data is None
 
